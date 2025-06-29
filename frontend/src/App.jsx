945c5013e761473e6f90c881a31619942ebaaa35
GAJRAJ SINGH <156221066+gajraj-singh@users.noreply.github.com>
2025-06-29 23:33:07 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Login from './pages/Login';
+import Login from './pages/login';
 import Navbar from './components/navbar';
 import Register from './pages/Register';
 import CreateProduct from './pages/ProductCreate';
